0acdf4d75bc73be9fbc1d234765b7e3920acd7f9
taotao <1290812122@qq.com>
2019-09-19 13:51:28 +0800

    更新

---
+++ OpenCVForUnity/Assets/Script/TestOpenCv.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using OpenCvSharp;
+
+public class TestOpenCv : MonoBehaviour
+{
+    // 摄像头设备名称
+    public string deviceName;
+    // 接收返回的图片数据
+    private WebCamTexture webCamtex;
+    // 将摄像头纹理渲染到RawImage
+    public RawImage rawImage;
+    // 完成人脸识别获取的图
+    public RawImage detectFaceImage;
+
+    // -----------人脸识别代码--------------
+    private CascadeClassifier haarCascade = null;
+    private Mat haarResult;
+    private byte[] bs;
+    private Texture2D rt;
+
+    void Start ()
+    {
+        // 加载官方训练集(人脸识别代码)
+        haarCascade = new CascadeClassifier(Application.streamingAssetsPath + "/haarcascades/haarcascade_frontalface_alt2.xml");
+        // 调用摄像头
+        StartCoroutine(startWebCam());
+    }
+
+    private void Update ()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        OnPlayFace();
+    }
+
+    /// <summary>
+    /// 打开摄像头
+    /// </summary>
+    public IEnumerator startWebCam()
+    {
+        // 调用摄像头信息
+        yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
+        if (Application.HasUserAuthorization(UserAuthorization.WebCam))
+        {
+            WebCamDevice[] devices = WebCamTexture.devices;
+            deviceName = devices[0].name;
+            webCamtex = new WebCamTexture(deviceName, 700, 700, 12);
+            webCamtex.Play();
+
+            rawImage.texture = webCamtex;
+        }
+        else
+        {
+            Debug.Log("没有找到摄像头设备");
+        }
+    }
+
+    // -----------人脸识别代码--------------
+    /// <summary>
+    /// 执行人脸检测
+    /// </summary>
+    public void OnPlayFace()
+    {
+        if(webCamtex != null)
+        {
+            haarResult = DetectFace(haarCascade, GetTexture2D(webCamtex));
+            bs = haarResult.ToBytes(".png");
+
+            rt = new Texture2D(webCamtex.width, webCamtex.height);
+            rt.LoadImage(bs);
+            rt.Apply();
+
+            detectFaceImage.texture = rt;
+        }
+    }
+
+    Mat result;
+    OpenCvSharp.Rect[] faces;
+    Mat src;
+    Mat gray = new Mat();
+    Size axes = new Size();
+
+    Point center = new Point();
+    Point upPoint = new Point();
+    Point downPoint = new Point();
+
+    /// <summary>
+    /// 调用OpenCvSharp处理人脸识别
+    /// </summary>
+    private Mat DetectFace(CascadeClassifier cascade, Texture2D tex2D_)
+    {
+        src = Mat.FromImageData(tex2D_.EncodeToPNG(), ImreadModes.Color);
+        result = src.Clone();
+        Cv2.CvtColor(src, gray, ColorConversionCodes.BGR2GRAY);
+        src = null;
+        // Detect faces (openCV库人脸识别函数)
+        faces = cascade.DetectMultiScale(gray, 1.08, 4, HaarDetectionType.ScaleImage, new Size(10, 10));
+
+        //高斯滤波
+        Mat resultsss = new Mat();
+        Cv2.GaussianBlur(src, resultsss, new Size(3, 3), 0, 0);
+
+        /*
+        // 给识别区域画出范围
+        Debug.Log(faces.Length);
+        for (int i = 0; i < faces.Length; i++)
+        {
+            // 画椭圆
+            //center.X = (int)(faces[i].X + faces[i].Width * 0.5);
+            //center.Y = (int)(faces[i].Y + faces[i].Height * 0.5);
+            //axes.Width = (int)(faces[i].Width * 0.5);
+            //axes.Height = (int)(faces[i].Height * 0.5);
+            //Cv2.Ellipse(result, center, axes, 0, 0, 360, new Scalar(0, 0, 255), 4);
+
+            // 画矩形
+            upPoint.X = faces[i].X;
+            upPoint.Y = faces[i].Y;
+            downPoint.X = (int)(faces[i].X + faces[i].Width);
+            downPoint.Y = (int)(faces[i].Y + faces[i].Height);
+            Cv2.Rectangle(result, upPoint, downPoint, new Scalar(0, 0, 255), 3, LineTypes.Link8, 0);
+        }
+        */
+        return result;
+    }
+
+    /// <summary>
+    /// 获取摄像头视频流一帧保存成Texture2D
+    /// </summary>
+    Texture2D temp;
+    Texture2D GetTexture2D(WebCamTexture wct)
+    {
+        temp = new Texture2D(wct.width, wct.height);
+        temp.SetPixels(wct.GetPixels());
+        temp.Apply();
+        return temp;
+    }
+}
---
+++ OpenCVForUnity/Assets/Script/TestOpenCvFeature.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using OpenCvSharp;
+
+public class TestOpenCvFeature : MonoBehaviour {
+    public Image m_srcImage;
+
+    void Start ()
+    {
+    }
+
+    void Update ()
+    {
+		
+	}
+
+    /// <summary>
+    /// 随机一个路径
+    /// </summary>
+    public void OnRandomPath()
+    {
+        List<string> pathList = new List<string>();
+        pathList.Add("/Textures/p2.jpg");
+        pathList.Add("/Textures/p3.jpg");
+        pathList.Add("/Textures/p5.jpg");
+        pathList.Add("/Textures/p6.jpg");
+
+        int index = Random.Range(0, pathList.Count);
+
+        OnOrb(pathList[index]);
+    }
+
+    //void Detect()
+    //{
+    //    var gray = new Mat(Application.streamingAssetsPath + "/Textures/p1.jpg", ImreadModes.Grayscale);
+    //    KeyPoint[] keyPoints = null;
+    //    using (var orb = ORB.Create(500))
+    //    {
+    //        keyPoints = orb.Detect(gray);
+    //        Debug.Log($"KeyPoint has {keyPoints.Length} items.");
+    //    }
+    //}
+
+    //void DetectAndCompute()
+    //{
+    //    var gray = new Mat(Application.streamingAssetsPath + "/Textures/p1.jpg", ImreadModes.Grayscale);
+    //    KeyPoint[] keyPoints = null;
+    //    using (var orb = ORB.Create(500))
+    //    using (Mat descriptor = new Mat())
+    //    {
+    //        orb.DetectAndCompute(gray, new Mat(), out keyPoints, descriptor);
+    //        Debug.Log($"keyPoints has {keyPoints.Length} items.");
+    //        Debug.Log($"descriptor has {descriptor.Rows} items.");
+    //    }
+    //}
+
+    Mat dstMat;
+    Texture2D t2d;
+    /// <summary>
+    /// Orb特征提取
+    /// </summary>
+    void OnOrb(string path2_)
+    {
+        Debug.Log(path2_);
+
+        Mat image01 = Cv2.ImRead(Application.streamingAssetsPath + "/Textures/p1.jpg");
+        Mat image02 = Cv2.ImRead(Application.streamingAssetsPath + path2_);
+
+        //灰度图转换
+        Mat image1 = new Mat(), image2 = new Mat();
+        Cv2.CvtColor(image01, image1, ColorConversionCodes.RGB2GRAY);
+        Cv2.CvtColor(image02, image2, ColorConversionCodes.RGB2GRAY);
+
+        KeyPoint[] keyPoint1 = null;
+        KeyPoint[] keyPoint2 = null;
+        using (ORB orb = ORB.Create(500))
+        using (Mat descriptor1 = new Mat())
+        using (Mat descriptor2 = new Mat())
+        using (var matcher = new BFMatcher())
+        {
+            //特征点提取并计算
+            orb.DetectAndCompute(image1, new Mat(), out keyPoint1, descriptor1);
+            orb.DetectAndCompute(image2, new Mat(), out keyPoint2, descriptor2);
+            Debug.Log("image1 keyPoints:  " + keyPoint1.Length + "   descriptor:  " + descriptor1.Rows);
+            Debug.Log("image2 keyPoints:  " + keyPoint2.Length + "   descriptor:  " + descriptor2.Rows);
+
+            //特征点匹配
+            //DMatch[] matchePoint = null;
+            //matchePoint = matcher.Match(descriptor2, descriptor1);
+            //Debug.Log(matchePoint.Length);
+
+            // Lowe's algorithm,获取优秀匹配点
+            DMatch[][] matchePoints = null;
+            DMatch[][] matchePointssss = null;
+            // 使用knnMatch最邻近匹配
+            matchePoints = matcher.KnnMatch(descriptor1, descriptor2, 2);
+
+            List<DMatch> GoodMatchePoints = new List<DMatch>();
+            for (int i = 0; i < matchePoints.Length; i++)
+            {
+                float minRatio = 0.9f;
+                float disRation = matchePoints[i][0].Distance / matchePoints[i][1].Distance;
+                if(disRation < minRatio)
+                    GoodMatchePoints.Add(matchePoints[i][0]);
+            }
+            
+            DMatch[] matchePointss = GoodMatchePoints.ToArray();
+
+            Debug.Log("手写文字图总特征点： " + descriptor2.Rows);
+            Debug.Log("匹配符合的文字特征点： " + matchePointss.Length);
+            
+            //float zongVa = (matchePoints.Length / 2);
+            Debug.Log("相识度比例：  " + ((float)matchePointss.Length / descriptor2.Rows));
+
+            dstMat = new Mat();
+            Cv2.DrawMatches(image01, keyPoint1, image02, keyPoint2, matchePointss, dstMat);
+            t2d = MatToTexture2D(dstMat);
+        }
+
+        Sprite dst_sp = Sprite.Create(t2d, new UnityEngine.Rect(0, 0, t2d.width, t2d.height), Vector2.zero);
+        m_srcImage.sprite = dst_sp;
+        m_srcImage.preserveAspect = true;
+    }
+
+    /// <summary>
+    /// Mat转Texture2D
+    /// </summary>
+    /// <param name="mat"></param>
+    /// <returns></returns>
+    public static Texture2D MatToTexture2D(Mat mat)
+    {
+        Texture2D t2d = new Texture2D(mat.Width, mat.Height);
+        t2d.LoadImage(mat.ToBytes());
+        t2d.Apply();
+        //赋值完后为什么要Apply
+        //因为在贴图更改像素时并不是直接对显存进行更改，而是在另外一个内存空间中更改，这时候GPU还会实时读取旧的贴图位置。
+        //当Apply后，CPU会告诉GPU你要换个地方读贴图了。
+        return t2d;
+    }
+}
